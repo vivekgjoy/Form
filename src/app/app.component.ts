e7605f1b729d9bb558f588fd7a739214f2a8fd61
vivekplato <109714586+vivekplato@users.noreply.github.com>
2022-08-24 09:48:10 +0530

    Form validation

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Form';
+  onsubmit(userForm:NgForm){
+    console.log(userForm)
+  }
+}
